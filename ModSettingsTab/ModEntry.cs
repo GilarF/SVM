0f221abd96c279160ad98ffd8613629758b7a79f
Gilar Fasulaki <gilar.fasulaki@gmail.com>
2019-12-05 13:00:41 +0200

    feat(ModSettingsTab): Add new mod

---
+++ ModSettingsTab/ModEntry.cs
@@ -0,0 +1,12 @@
+﻿using StardewModdingAPI;
+
+namespace ModSettingsTab
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            
+        }
+    }
+}
