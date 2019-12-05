95420bc42835ac9463ccaefd722bcb771c763c98
Gilar Fasulaki <gilar.fasulaki@gmail.com>
2019-12-05 21:44:12 +0200

    docs(ModEntry): add docs for ModEntry
    Signed-off-by: Gilar Fasulaki <gilar.fasulaki@gmail.com>

--- ModSettingsTab/ModEntry.cs
+++ ModSettingsTab/ModEntry.cs
@@ -6,10 +6,26 @@ namespace ModSettingsTab
 {
     public class ModEntry : Mod
     {
+        /// <summary>
+        /// SMAPI helper
+        /// </summary>
         public new static IModHelper Helper;
+
+        /// <summary>
+        /// SMAPI monitor
+        /// see: https://stardewvalleywiki.com/Modding:Modder_Guide/APIs/Logging
+        /// </summary>
         public static IMonitor Console;
+
+        /// <summary>
+        /// SMAPI internationalization helper
+        /// </summary>
         public static ITranslationHelper I18N;
 
+        /// <summary>
+        /// mod entry point
+        /// </summary>
+        /// <param name="helper"></param>
         public override void Entry(IModHelper helper)
         {
             Helper = helper;
@@ -19,6 +35,12 @@ namespace ModSettingsTab
             Helper.Events.Display.MenuChanged += MenuChanged;
         }
 
+        /// <summary>
+        /// handler of the change event of the active menu
+        /// replaces the settings tab
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void MenuChanged(object sender, MenuChangedEventArgs e)
         {
             if (e.NewMenu is GameMenu menu)
