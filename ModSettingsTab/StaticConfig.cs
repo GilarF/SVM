0767b0ec1ee1103531f24574f3ae13d7654398f8
Gilar Fasulaki <gilar.fasulaki@gmail.com>
2019-12-06 00:31:39 +0200

    feat: add StaticConfig
    Signed-off-by: Gilar Fasulaki <gilar.fasulaki@gmail.com>

--- ModSettingsTab/StaticConfig.cs
+++ ModSettingsTab/StaticConfig.cs
@@ -1,15 +1,81 @@
 using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
+using Newtonsoft.Json.Linq;
 
 namespace ModSettingsTab
 {
     public class StaticConfig : INotifyPropertyChanged, IEnumerable
     {
+        private readonly Dictionary<string, JToken> _properties = new Dictionary<string, JToken>();
+        private readonly JObject _config;
+
         public event PropertyChangedEventHandler PropertyChanged;
-        
-        public IEnumerator GetEnumerator()
+
+        public override string ToString()
         {
-            throw new System.NotImplementedException();
+            return _config.ToString();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _properties.GetEnumerator();
+        }
+
+        public StaticConfig(JObject config)
+        {
+            _config = config;
+            ParseProperties(_config);
+        }
+
+        public bool ContainsKey(string key)
+        {
+            return _properties.ContainsKey(key);
+        }
+
+        public void Remove(string key)
+        {
+            _properties.Remove(key);
+        }
+
+        public JToken this[string key]
+        {
+            get => !_properties.ContainsKey(key) ? null : _properties[key];
+            set
+            {
+                if (!_properties.ContainsKey(key))
+                    return;
+                _config.SelectToken(key).Replace(value);
+                _properties[key] = _config.SelectToken(key);
+                OnPropertyChanged(key);
+            }
+        }
+
+        private void OnPropertyChanged(string name)
+        {
+            var propertyChanged = PropertyChanged;
+            propertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private void ParseProperties(JToken obj)
+        {
+            switch (obj.Type)
+            {
+                case JTokenType.Object:
+                    ((JObject) obj).Properties().ToList()
+                        .ForEach(p => ParseProperties(p.Value));
+                    break;
+                case JTokenType.Array:
+                    obj.Children().ToList().ForEach(ParseProperties);
+                    break;
+                case JTokenType.Integer:
+                case JTokenType.Float:
+                case JTokenType.String:
+                case JTokenType.Boolean:
+                    _properties.Add(obj.Path, obj);
+                    break;
+            }
         }
     }
 }
