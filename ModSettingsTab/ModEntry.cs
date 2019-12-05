eef13994ff83abae3d3aa024da5b5571a150ef95
Gilar Fasulaki <gilar.fasulaki@gmail.com>
2019-12-05 21:13:58 +0200

    feat(ModEntry): add MenuChanged event handler
    add static field
    Helper, Console, I18n
    
    Signed-off-by: Gilar Fasulaki <gilar.fasulaki@gmail.com>

--- ModSettingsTab/ModEntry.cs
+++ ModSettingsTab/ModEntry.cs
@@ -1,12 +1,30 @@
 ﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley.Menus;
 
 namespace ModSettingsTab
 {
     public class ModEntry : Mod
     {
+        public new static IModHelper Helper;
+        public static IMonitor Console;
+        public static ITranslationHelper I18N;
+
         public override void Entry(IModHelper helper)
         {
-            
+            Helper = helper;
+            Console = Monitor;
+            I18N = helper.Translation;
+
+            Helper.Events.Display.MenuChanged += MenuChanged;
+        }
+
+        private void MenuChanged(object sender, MenuChangedEventArgs e)
+        {
+            if (e.NewMenu is GameMenu menu)
+            {
+//                menu.tabs.Insert(GameMenu.optionsTab, new OptionsPage());
+            }
         }
     }
 }
