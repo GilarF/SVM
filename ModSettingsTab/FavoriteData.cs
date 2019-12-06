c73d4de99acf2044a3ce6b608ea3f77c2d35e7d1
Gilar Fasulaki <gilar.fasulaki@gmail.com>
2019-12-06 18:33:02 +0200

    docs(FavoriteData.cs): add docs
    Signed-off-by: Gilar Fasulaki <gilar.fasulaki@gmail.com>

--- ModSettingsTab/FavoriteData.cs
+++ ModSettingsTab/FavoriteData.cs
@@ -3,39 +3,63 @@ using System.Timers;
 
 namespace ModSettingsTab
 {
+    /// <summary>
+    /// saves the mod to favorites
+    /// </summary>
     public static class FavoriteData
     {
-        private static Dictionary<string, bool> _favorite;
-        private static readonly Timer _saveTimer;
+        /// <summary>
+        /// collection of selected mods
+        /// </summary>
+        private static readonly Dictionary<string, bool> Favorite;
+
+        /// <summary>
+        /// save timer, anti-click protection
+        /// </summary>
+        private static readonly Timer SaveTimer;
 
         static FavoriteData()
         {
-            _saveTimer = new Timer(2000.0)
+            SaveTimer = new Timer(2000.0)
             {
                 Enabled = false,
                 AutoReset = false
             };
-            _saveTimer.Elapsed += (t, e) =>
-                ModEntry.Helper.Data.WriteJsonFile("favorite.json", _favorite);
+            SaveTimer.Elapsed += (t, e) =>
+                ModEntry.Helper.Data.WriteJsonFile("favorite.json", Favorite);
 
-            _favorite =
+            Favorite =
                 ModEntry.Helper.Data.ReadJsonFile<Dictionary<string, bool>>("favorite.json")
                 ?? new Dictionary<string, bool>();
         }
 
+        /// <summary>
+        /// Checks if the mod is bookmarked
+        /// </summary>
+        /// <param name="uniqueId">
+        /// unique mod identifier
+        /// </param>
+        /// <returns></returns>
         public static bool IsFavorite(string uniqueId)
         {
-            return _favorite.ContainsKey(uniqueId) && _favorite[uniqueId];
+            return Favorite.ContainsKey(uniqueId) && Favorite[uniqueId];
         }
 
+        /// <summary>
+        /// changes bookmark state
+        /// </summary>
+        /// <param name="uniqueId">
+        /// unique mod identifier
+        /// </param>
+        /// <param name="status"></param>
         public static void ChangeStatus(string uniqueId, bool status)
         {
-            if (_favorite.ContainsKey(uniqueId))
-                _favorite[uniqueId] = status;
+            if (Favorite.ContainsKey(uniqueId))
+                Favorite[uniqueId] = status;
             else
-                _favorite.Add(uniqueId, status);
-            _saveTimer.Stop();
-            _saveTimer.Start();
+                Favorite.Add(uniqueId, status);
+            SaveTimer.Stop();
+            SaveTimer.Start();
         }
     }
 }
