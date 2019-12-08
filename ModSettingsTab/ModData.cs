1cfcaf1a9478415766e6ba474409ff5ec6628612
Gilar Fasulaki <gilar.fasulaki@gmail.com>
2019-12-08 17:45:14 +0200

    feat(Integration): add integration & async load

--- ModSettingsTab/ModData.cs
+++ ModSettingsTab/ModData.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ModSettingsTab.Framework.Integration;
+using StardewModdingAPI;
 using StardewValley.Menus;
 using OptionsElement = ModSettingsTab.Framework.Components.OptionsElement;
 
@@ -19,6 +21,14 @@ namespace ModSettingsTab
         public static ModList ModList { get; private set; }
 
         /// <summary>
+        /// predefined collection of mod settings
+        /// </summary>
+        /// <remarks>
+        /// Dictionary&lt;string:UniqueId, ModIntegrationSettings&gt;
+        /// </remarks>
+        public static Dictionary<string, ModIntegrationSettings> PredefinedIntegration;
+
+        /// <summary>
         /// list of all modification settings
         /// </summary>
         public static List<OptionsElement> Options;
@@ -33,21 +43,8 @@ namespace ModSettingsTab
         /// </summary>
         public static void Init()
         {
-            ModList = new ModList();
-            Options = ModList.SelectMany(mod => mod.Value.Options).ToList();
-            UpdateFavoriteOptions();
-        }
-
-        /// <summary>
-        /// asynchronously updates the list of settings of selected mods
-        /// </summary>
-        private static void UpdateFavoriteOptions()
-        {
-            Task.Run(() =>
-            {
-                FavoriteOptions = ModList.Where(mod => mod.Value.Favorite)
-                    .SelectMany(mod => mod.Value.Options).ToList();
-            });
+            LoadOptionsAsync();
+            LoadIntegrationsAsync();
         }
 
         /// <summary>
@@ -60,7 +57,57 @@ namespace ModSettingsTab
         public static void ChangeFavoriteMod(string uniqueId, bool status)
         {
             ModList[uniqueId].Favorite = status;
-            UpdateFavoriteOptions();
+            UpdateFavoriteOptionsAsync();
+        }
+
+        private static async void LoadOptionsAsync()
+        {
+            await LoadOptions();
+            ModEntry.Console.Log($"Load {ModList.Count} mods and {Options.Count} Options", LogLevel.Info);
+        }
+
+        private static async void LoadIntegrationsAsync()
+        {
+            await LoadIntegrations();
+            ModEntry.Console.Log($"Load {PredefinedIntegration.Count} Integrations", LogLevel.Info);
+        }
+
+        /// <summary>
+        /// asynchronously updates the list of settings of selected mods
+        /// </summary>
+        private static async void UpdateFavoriteOptionsAsync()
+        {
+            await LoadFavoriteOptions();
+        }
+
+        private static Task LoadIntegrations()
+        {
+            return Task.Run(() =>
+            {
+                PredefinedIntegration =
+                    ModEntry.Helper.Data.ReadJsonFile<Dictionary<string, ModIntegrationSettings>>(
+                        "data/integration.json") ?? new Dictionary<string, ModIntegrationSettings>();
+            });
+        }
+
+        private static Task LoadOptions()
+        {
+            return Task.Run(() =>
+            {
+                ModList = new ModList();
+                Options = ModList.SelectMany(mod => mod.Value.Options).ToList();
+                FavoriteOptions = ModList.Where(mod => mod.Value.Favorite)
+                    .SelectMany(mod => mod.Value.Options).ToList();
+            });
+        }
+
+        private static Task LoadFavoriteOptions()
+        {
+            return Task.Run(() =>
+            {
+                FavoriteOptions = ModList.Where(mod => mod.Value.Favorite)
+                    .SelectMany(mod => mod.Value.Options).ToList();
+            });
         }
     }
 }
