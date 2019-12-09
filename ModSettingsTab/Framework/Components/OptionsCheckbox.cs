fe91af74dc2705615c0623974e350916d5d4d54c
Gilar Fasulaki <gilar.fasulaki@gmail.com>
2019-12-09 21:08:03 +0200

    feat(Components): add checkBox

---
+++ ModSettingsTab/Framework/Components/OptionsCheckbox.cs
@@ -0,0 +1,39 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Newtonsoft.Json.Linq;
+using StardewValley;
+
+namespace ModSettingsTab.Framework.Components
+{
+    public class OptionsCheckbox : OptionsElement
+    {
+        private static readonly Rectangle SourceRectUnchecked = new Rectangle(227, 425, 9, 9);
+        private static readonly Rectangle SourceRectChecked = new Rectangle(236, 425, 9, 9);
+        public bool AsString { get; set; }
+        private bool _isChecked;
+
+        public OptionsCheckbox(string name,
+            string modId,
+            string label,
+            StaticConfig config,
+            Point slotSize)
+            : base(name, modId, label, config, 32, slotSize.Y / 2)
+        {
+            _isChecked = config[name].ToString().ToLower() == "true";
+        }
+
+        public override void ReceiveLeftClick(int x, int y)
+        {
+            Game1.playSound("drumkit6");
+            base.ReceiveLeftClick(x, y);
+            _isChecked = !_isChecked;
+            Config[Name] = AsString ? (JToken) _isChecked.ToString().ToLower() : (JToken) _isChecked;
+        }
+
+        public override void Draw(SpriteBatch b, int slotX, int slotY)
+        {
+            b.Draw(Game1.mouseCursors, new Vector2(slotX + Bounds.X, slotY + Bounds.Y), _isChecked ? SourceRectChecked : SourceRectUnchecked, Color.White * (GreyedOut ? 0.33f : 1f), 0.0f, Vector2.Zero, 4f, SpriteEffects.None, 0.4f);
+            Utility.drawTextWithShadow(b, Label, Game1.dialogueFont, new Vector2(slotX + Bounds.X + Bounds.Width + 8, slotY + Bounds.Y), GreyedOut ? Game1.textColor * 0.33f : Game1.textColor, 1f, 0.1f);
+        }
+    }
+}
