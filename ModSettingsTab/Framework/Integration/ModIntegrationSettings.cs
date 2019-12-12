74cac5ee40fa6da5a28bf99bf2b7b6618527adcb
Gilar Fasulaki <gilar.fasulaki@gmail.com>
2019-12-12 15:46:04 +0200

    feat(Integration): add default val

--- ModSettingsTab/Framework/Integration/ModIntegrationSettings.cs
+++ ModSettingsTab/Framework/Integration/ModIntegrationSettings.cs
@@ -4,7 +4,7 @@ namespace ModSettingsTab.Framework.Integration
 {
     public class ModIntegrationSettings
     {
-        public I18n Description { get; set; }
-        public List<Param> Config { get; set; }
+        public I18n Description { get; set; } = new I18n();
+        public List<Param> Config { get; set; } = new List<Param>();
     }
 }
