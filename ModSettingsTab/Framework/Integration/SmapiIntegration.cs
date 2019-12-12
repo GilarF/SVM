60789edb7409cdb847dcc0fde118efe8ef7f8660
Gilar Fasulaki <gilar.fasulaki@gmail.com>
2019-12-12 15:50:43 +0200

    feat(Integration): add smapi integration

--- ModSettingsTab/Framework/Integration/SmapiIntegration.cs
+++ ModSettingsTab/Framework/Integration/SmapiIntegration.cs
@@ -1,10 +1,15 @@
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 using ModSettingsTab.Framework.Components;
 using ModSettingsTab.Menu;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using StardewModdingAPI;
+using StardewValley;
 
 namespace ModSettingsTab.Framework.Integration
 {
@@ -12,76 +17,88 @@ namespace ModSettingsTab.Framework.Integration
     {
         public readonly List<OptionsElement> Options = new List<OptionsElement>();
         private readonly StaticConfig _staticConfig;
-        
+
         public SmapiIntegration()
         {
-            var configPath = Path.Combine(Constants.ExecutionPath,"smapi-internal/config.json");
+            var configPath = Path.Combine(Constants.ExecutionPath, "smapi-internal/config.json");
             if (!File.Exists(configPath))
             {
-                ModEntry.Console.Log("SMAPI Config not found? :)",LogLevel.Error);
+                ModEntry.Console.Log("SMAPI Config not found? :)", LogLevel.Error);
                 return;
             }
-            var jObj = JObject.Parse(File.ReadAllText(configPath));
+
+            var json = File.ReadAllText(configPath);
+            json = Regex.Replace(json, "\\/{2}\"(ParanoidWarnings|UseBetaChannel)\": true,", "\"$1\": false,");
+            var jObj = JObject.Parse(json);
             _staticConfig = new StaticConfig(configPath, jObj);
 
             InitOptions();
         }
+
         private void InitOptions()
         {
+            const string uniqueId = "Pathoschild.SMAPI";
+            var lang = LocalizedContentManager.CurrentLanguageCode;
             
+            var json = File.ReadAllText(Path.Combine(ModEntry.Helper.DirectoryPath, "data/SmapiIntegrations.json"));
+            var integration = JsonConvert.DeserializeObject<ModIntegrationSettings>(json) ??
+                              new ModIntegrationSettings();
             Options.Add(new SmapiHeading(BaseOptionsModPage.SlotSize)
             {
                 Label = "SMAPI",
-                HoverText = "The default values are mirrored in StardewModdingAPI.Framework.Models.SConfig to log custom changes.",
+                HoverText = integration.Description[lang] ??
+                            "The default values are mirrored in StardewModdingAPI.Framework.Models.SConfig to log custom changes.",
                 HoverTitle = "Pathoschild",
             });
             foreach (KeyValuePair<string, JToken> param in _staticConfig)
             {
-                StaticInit(param.Value, param.Key);
+                var iOpt = integration.Config.FindLast(o => o.Name == param.Key);
+                if (iOpt == null)
+                {
+                    StaticInit(param.Value, param.Key);
+                    continue;
+                }
+
+                if (iOpt.Ignore) continue;
+                switch (iOpt.Type)
+                {
+                    case ParamType.CheckBox:
+                        Options.Add(new OptionsCheckbox(param.Key, uniqueId, iOpt.Label ?? param.Key,
+                            _staticConfig,
+                            BaseOptionsModPage.SlotSize)
+                        {
+                            HoverText = iOpt.Description?[lang],
+                            ShowTooltip = !string.IsNullOrEmpty(iOpt.Description?[lang])
+                        });
+                        continue;
+                    case ParamType.DropDown:
+                        Options.Add(new OptionsDropDown(param.Key, uniqueId, iOpt.Label ?? param.Key,
+                            _staticConfig, BaseOptionsModPage.SlotSize, iOpt.DropDownOptions)
+                        {
+                            HoverText = iOpt.Description?[lang],
+                            ShowTooltip = !string.IsNullOrEmpty(iOpt.Description?[lang])
+                        });
+                        continue;
+                    case ParamType.PlusMinus:
+                        Options.Add(new OptionsPlusMinus(param.Key, uniqueId, iOpt.Label ?? param.Key,
+                            _staticConfig, BaseOptionsModPage.SlotSize, iOpt.PlusMinusOptions??new List<string>())
+                        {
+                            HoverText = iOpt.Description?[lang],
+                            ShowTooltip = !string.IsNullOrEmpty(iOpt.Description?[lang])
+                        });
+                        continue;
+                    default:
+                        StaticInit(param.Value, param.Key);
+                        continue;
+                }
             }
 
             void StaticInit(JToken opt, string name)
             {
-                var uniqueId = "Pathoschild.SMAPI";
                 switch (opt.Type)
                 {
-                    case JTokenType.Integer:
-                        Options.Add(new OptionsTextBox(name, uniqueId, name, _staticConfig, BaseOptionsModPage.SlotSize,
-                            false, true));
-                        break;
-                    case JTokenType.Float:
-                        Options.Add(new OptionsTextBox(name, uniqueId, name, _staticConfig, BaseOptionsModPage.SlotSize,
-                            true));
-                        break;
                     case JTokenType.String:
                         var str = opt.ToString().Trim();
-
-                        // check int
-                        if (int.TryParse(str, out _))
-                        {
-                            Options.Add(new OptionsTextBox(name, uniqueId, name, _staticConfig,
-                                BaseOptionsModPage.SlotSize, false, true, true));
-                            break;
-                        }
-                        // check bool
-                        if (str.ToLower().Equals("true") || str.ToLower().Equals("false"))
-                        {
-                            Options.Add(new OptionsCheckbox(name, uniqueId, name, _staticConfig,
-                                BaseOptionsModPage.SlotSize)
-                            {
-                                AsString = true
-                            });
-                            break;
-                        }
-
-                        // check float
-                        if (float.TryParse(str, NumberStyles.Any, CultureInfo.CreateSpecificCulture("en-US"), out _))
-                        {
-                            Options.Add(new OptionsTextBox(name, uniqueId, name, _staticConfig,
-                                BaseOptionsModPage.SlotSize, true, false, true));
-                            break;
-                        }
-
                         Options.Add(
                             new OptionsTextBox(name, uniqueId, name, _staticConfig, BaseOptionsModPage.SlotSize));
                         break;
