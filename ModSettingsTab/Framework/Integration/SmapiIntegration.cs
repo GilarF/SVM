a599f80bacee64626de9724ef7fcd3cdbfaf9ac2
Gilar Fasulaki <gilar.fasulaki@gmail.com>
2019-12-16 14:44:12 +0200

    feat(SmapiI9N): change init

--- ModSettingsTab/Framework/Integration/SmapiIntegration.cs
+++ ModSettingsTab/Framework/Integration/SmapiIntegration.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
@@ -36,73 +37,33 @@ namespace ModSettingsTab.Framework.Integration
         {
             const string uniqueId = "Pathoschild.SMAPI";
             var lang = LocalizedContentManager.CurrentLanguageCode;
+            var nI9NPath = Path.Combine(ModEntry.Helper.DirectoryPath, "data/I9N/SmapiIntegrations.json");
+            ModIntegrationSettings nI9N = null;
+            try
+            {
+                nI9N = File.Exists(nI9NPath)
+                    ? JsonConvert.DeserializeObject<ModIntegrationSettings>(File.ReadAllText(nI9NPath))
+                    : null;
+            }
+            catch (Exception e)
+            {
+                ModEntry.Console.Log(e.Message);
+            }
             
-            var json = File.ReadAllText(Path.Combine(ModEntry.Helper.DirectoryPath, "data/SmapiIntegrations.json"));
-            var integration = JsonConvert.DeserializeObject<ModIntegrationSettings>(json) ??
-                              new ModIntegrationSettings();
             Options.Add(new SmapiHeading(BaseOptionsModPage.SlotSize)
             {
                 Label = "SMAPI",
-                HoverText = integration.Description[lang] ??
+                HoverText = nI9N?.Description[lang] ??
                             "The default values are mirrored in StardewModdingAPI.Framework.Models.SConfig to log custom changes.",
                 HoverTitle = "Pathoschild",
             });
             foreach (KeyValuePair<string, JToken> param in _staticConfig)
             {
-                var iOpt = integration.Config.FindLast(o => o.Name == param.Key);
-                if (iOpt == null)
-                {
-                    StaticInit(param.Value, param.Key);
-                    continue;
-                }
-
-                if (iOpt.Ignore) continue;
-                switch (iOpt.Type)
-                {
-                    case ParamType.CheckBox:
-                        Options.Add(new OptionsCheckbox(param.Key, uniqueId, iOpt.Label ?? param.Key,
-                            _staticConfig,
-                            BaseOptionsModPage.SlotSize)
-                        {
-                            HoverText = iOpt.Description?[lang],
-                            ShowTooltip = !string.IsNullOrEmpty(iOpt.Description?[lang])
-                        });
-                        continue;
-                    case ParamType.DropDown:
-                        Options.Add(new OptionsDropDown(param.Key, uniqueId, iOpt.Label ?? param.Key,
-                            _staticConfig, BaseOptionsModPage.SlotSize, iOpt.DropDownOptions)
-                        {
-                            HoverText = iOpt.Description?[lang],
-                            ShowTooltip = !string.IsNullOrEmpty(iOpt.Description?[lang])
-                        });
-                        continue;
-                    case ParamType.PlusMinus:
-                        Options.Add(new OptionsPlusMinus(param.Key, uniqueId, iOpt.Label ?? param.Key,
-                            _staticConfig, BaseOptionsModPage.SlotSize, iOpt.PlusMinusOptions??new List<string>())
-                        {
-                            HoverText = iOpt.Description?[lang],
-                            ShowTooltip = !string.IsNullOrEmpty(iOpt.Description?[lang])
-                        });
-                        continue;
-                    default:
-                        StaticInit(param.Value, param.Key);
-                        continue;
-                }
-            }
-
-            void StaticInit(JToken opt, string name)
-            {
-                switch (opt.Type)
-                {
-                    case JTokenType.String:
-                        Options.Add(
-                            new OptionsTextBox(name, uniqueId, name, _staticConfig, BaseOptionsModPage.SlotSize));
-                        break;
-                    case JTokenType.Boolean:
-                        Options.Add(new OptionsCheckbox(name, uniqueId, name, _staticConfig,
-                            BaseOptionsModPage.SlotSize));
-                        break;
-                }
+                var option = Mod.CreateOption(
+                    param.Key, uniqueId, _staticConfig,
+                    BaseOptionsModPage.SlotSize, null, nI9N);
+                if (option == null) continue;
+                Options.Add(option);
             }
         }
     }
